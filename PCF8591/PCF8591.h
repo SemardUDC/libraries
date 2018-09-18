8fed06c344e2575329648d6c4bf8de2d80e0535f
Bejojuda <juanma.712@hotmail.com>
2018-09-18 14:25:51 -0500

    Lectura analogica de valores por medio del PCF8591

--- PCF8591/PCF8591.h
+++ PCF8591/PCF8591.h
@@ -9,40 +9,42 @@
 	#endif
 	#include <math.h>
 
-	#define AIN0 B00000000
-	#define AIN1 B00000001
-	#define AIN2 B00000010
-	#define AIN3 B00000011
+	#define ENTRADAA0 B00000000
+	#define ENTRADAA1 B00000001
+	#define ENTRADAA2 B00000010
+	#define ENTRADAA3 B00000011
 
-	#define ENABLE_OUTPUT B01000000
-	#define DISABLE_OUTPUT B01000000
+	#define ENTR_FINAL_UNITARIO B00000000
 
-	#define OUTPUT_MASK B01000000
+	#define HABILITAR_SALIDA B01000000
+	#define DESHABILITAR_SALIDA B01000000
+
+	#define MASCARA_DE_SALIDA B01000000
 
 	class PCF8591 {
 		public:
-			struct AnalogInput {
-			   uint8_t ain0;
-			   uint8_t ain1;
-			   uint8_t ain2;
-			   uint8_t ain3;
-			} analogInput;
+			struct EntradaAnalogica {
+			   uint8_t ea0;
+			   uint8_t ea1;
+			   uint8_t ea2;
+			   uint8_t ea3;
+			} entradaAnalogica;
 
-			PCF8591(uint8_t address);
+			PCF8591(uint8_t direccion);
 
 			#ifndef __AVR
-				PCF8591(uint8_t address, uint8_t sda, uint8_t scl);
+				PCF8591(uint8_t direccion, uint8_t sda, uint8_t scl);
 			#endif
 
-			void begin(void);
-			uint8_t analogRead(uint8_t channel, byte readType = SINGLE_ENDED_INPUT);
+			void iniciar(void);
+			uint8_t lecturaAnalogica(uint8_t canal, byte leerTipo = ENTR_FINAL_UNITARIO);
 
 		private:
-			uint8_t _address;
+			uint8_t _direccion;
 			uint8_t _sda = SDA;
 			uint8_t _scl = SCL;
 
-			byte _outputStatus = DISABLE_OUTPUT;
+			byte _estadoDeSalida = DESHABILITAR_SALIDA;
 	};
 
 #endif
