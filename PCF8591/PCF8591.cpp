5a06a0ede4723c6c1d67b1a75bd23db4c9ae2106
Humberto Leal <humberto.adolfo@hotmail.com>
2018-10-18 20:51:42 -0500

    Soporte de PCF8591 para multiples buses

--- PCF8591/PCF8591.cpp
+++ PCF8591/PCF8591.cpp
@@ -1,35 +1,42 @@
 #include "PCF8591.h"
 #include "Wire.h"
 
-PCF8591::PCF8591(uint8_t direccion){
+PCF8591::PCF8591(uint8_t direccion)
+{
 	_direccion = direccion;
 };
 #ifndef __AVR
-	PCF8591::PCF8591(uint8_t direccion, uint8_t sda, uint8_t scl){
-		_direccion = direccion;
-		_sda = sda;
-		_scl = scl;
-	};
+PCF8591::PCF8591(uint8_t direccion, uint8_t sda, uint8_t scl, bool multiple)
+{
+	_direccion = direccion;
+	_sda = sda;
+	_scl = scl;
+	_multiple = multiple;
+};
 #endif
 
-void PCF8591::iniciar(){
-	#ifndef __AVR
-		Wire.begin(_sda, _scl);
-	#else
-		Wire.begin();
-	#endif
+void PCF8591::iniciar()
+{
+#ifndef __AVR
+	Wire.begin(_sda, _scl);
+#else
+	Wire.begin();
+#endif
 }
 
-uint8_t PCF8591::lecturaAnalogica(uint8_t canal, byte leerTipo){
+uint8_t PCF8591::lecturaAnalogica(uint8_t canal, byte leerTipo)
+{
+	if (_multiple)
+		Wire.begin(_sda, _scl);
 	Wire.beginTransmission(_direccion);
 
-	byte operacion = canal | leerTipo| (_estadoDeSalida&MASCARA_DE_SALIDA);
+	byte operacion = canal | leerTipo | (_estadoDeSalida & MASCARA_DE_SALIDA);
 
 	Wire.write(operacion);
 	Wire.endTransmission();
 	Wire.requestFrom(_direccion, (uint8_t)2);
 	Wire.read();
-	uint8_t lectura=Wire.read();
+	uint8_t lectura = Wire.read();
 
 	return lectura;
 };
--- PCF8591/PCF8591.h
+++ PCF8591/PCF8591.h
@@ -1,50 +1,58 @@
 #ifndef PCF8591_h
-	#define PCF8591_h
-	#include "Wire.h"
+#define PCF8591_h
+#include "Wire.h"
 
-	#if ARDUINO >= 100
-		#include "Arduino.h"
-	#else
-		#include "WProgram.h"
-	#endif
-	#include <math.h>
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <math.h>
 
-	#define ENTRADAA0 B00000000
-	#define ENTRADAA1 B00000001
-	#define ENTRADAA2 B00000010
-	#define ENTRADAA3 B00000011
+#define ENTRADAA0 B00000000
+#define ENTRADAA1 B00000001
+#define ENTRADAA2 B00000010
+#define ENTRADAA3 B00000011
 
-	#define ENTR_FINAL_UNITARIO B00000000
+#define ENTR_FINAL_UNITARIO B00000000
 
-	#define HABILITAR_SALIDA B01000000
-	#define DESHABILITAR_SALIDA B01000000
+#define HABILITAR_SALIDA B01000000
+#define DESHABILITAR_SALIDA B01000000
 
-	#define MASCARA_DE_SALIDA B01000000
+#define MASCARA_DE_SALIDA B01000000
 
-	class PCF8591 {
-		public:
-			struct EntradaAnalogica {
-			   uint8_t ea0;
-			   uint8_t ea1;
-			   uint8_t ea2;
-			   uint8_t ea3;
-			} entradaAnalogica;
+class PCF8591
+{
+  public:
+	struct EntradaAnalogica
+	{
+		uint8_t ea0;
+		uint8_t ea1;
+		uint8_t ea2;
+		uint8_t ea3;
+	} entradaAnalogica;
 
-			PCF8591(uint8_t direccion);
+	PCF8591(uint8_t direccion);
 
-			#ifndef __AVR
-				PCF8591(uint8_t direccion, uint8_t sda, uint8_t scl);
-			#endif
+#ifndef __AVR
+	PCF8591(uint8_t direccion, uint8_t sda, uint8_t scl, bool multiple = false);
+#endif
 
-			void iniciar(void);
-			uint8_t lecturaAnalogica(uint8_t canal, byte leerTipo = ENTR_FINAL_UNITARIO);
+	void iniciar(void);
+	uint8_t lecturaAnalogica(uint8_t canal, byte leerTipo = ENTR_FINAL_UNITARIO);
 
-		private:
-			uint8_t _direccion;
-			uint8_t _sda = SDA;
-			uint8_t _scl = SCL;
+  private:
+	uint8_t _direccion;
+	uint8_t _sda = SDA;
+	uint8_t _scl = SCL;
 
-			byte _estadoDeSalida = DESHABILITAR_SALIDA;
-	};
+	// If multiple I2C buses are being used
+	// by the ESP, it stablishes the execution 
+	// of Wire.begin for every read/write 
+	// operation with the _sda and _scl configured.
+	bool _multiple;
+
+	byte _estadoDeSalida = DESHABILITAR_SALIDA;
+};
 
 #endif
