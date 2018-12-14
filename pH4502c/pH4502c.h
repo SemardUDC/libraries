62fe63d569eb6478b6e2fa15e40620e353624274
Humberto Leal <humberto.adolfo@hotmail.com>
2018-12-14 09:44:23 -0500

    Modificaciones clase pH4502c: Soporte opcional para PCF8591, adicion de constructores y cambio de api de metodo lecturaAnalogica->handle

--- pH4502c/pH4502c.h
+++ pH4502c/pH4502c.h
@@ -8,16 +8,17 @@ class pH4502c {
     uint8_t medida;
     float_t pHVol;
     float_t pHVal;
-    PCF8591& pcf;
+    PCF8591 *pcf;
     void(*callback)(float_t);
     uint8_t pin;
 
   public:
     //Retorna valores bi-decimales de entre 0-14
     //De acuerdo a la escala de pH
-    float_t lecturaAnalogica();
-    pH4502c(PCF8591 _pcf, uint8_t _pin, void(*_callback)(float_t) );
-
+    float_t handle();
+    pH4502c(PCF8591& _pcf, uint8_t _pin, void(*_callback)(float_t) );
+    pH4502c(uint8_t _pin, void(*_callback)(float_t) );
+    ~pH4502c();
 
 };
 #endif
